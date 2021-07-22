8b56d178c768842683f9e258d7d84ff0cc4622b9
JackLiang1003 <87572699+JackLiang1003@users.noreply.github.com>
2021-07-22 21:44:47 +0800

    androidexe

---
+++ MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        TextView tv_one =  findViewById(R.id.tv_one);
+//        tv_one.setText("LY");
+    }
+}
